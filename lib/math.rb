d43d7469dab72bed234559a443b5dd79f213fb23
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-21 06:48:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+  2+2
 end
 
 def subtraction(num1, num2)
+  4-3
 end
 
 def division(num1, num2)
+  10/2
 end
 
 def multiplication(num1, num2)
+  5*5
 end
 
 def modulo(num1, num2)
+  9%2
 end
 
 def square_root(num)
+  Math.sqrt(49)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  3+((2x2) / 2 )
   #Hint:  __ + (( __ * __ ) / __ )
 end
